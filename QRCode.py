28c56bc80fa0e7e48d0cc7526da5f1b3137c3738
olfasaad <olfasaad99@gmail.com>
2024-04-17 23:33:58 +0200

    first commit

---
+++ QRCode.py
@@ -0,0 +1,16 @@
+import qrcode
+import image 
+
+qr=qrcode.QRCode(
+    version=15,
+    box_size=10,
+    border=5 
+
+)
+
+data="https://www.linkedin.com/feed/"
+
+qr.add_data(data)
+qr.make(fit=True)
+img =qr.make_image(fill="black",color="white")
+img.save("img.png")
